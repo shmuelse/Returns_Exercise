774fbab8ca433da95314af2dfd207f9c559def42
shmuelse <44799431+shmuelse@users.noreply.github.com>
2020-09-01 20:23:43 +0300

    Work on CRUD

--- ReturnExercise.py
+++ ReturnExercise.py
@@ -10,47 +10,82 @@ import sqlite3
 #     print(my_data)
 
 
-# connect to database
-connection = sqlite3.connect('returns.db')
+# CREATE
+def add_customer(customer):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
 
-# create a cursor
-cursor = connection.cursor()
+    # create a cursor
+    cursor = connection.cursor()
 
-# create Customers table
-cursor.execute("""CREATE TABLE customers (
-    customer_id INTEGER PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT,
-    email_address TEXT,
-    phone_number TEXT,
-    address TEXT,
-    geo_area TEXT,
-    active TEXT
-)
-""")
+    # add the customer to the table
+    cursor.execute("INSERT INTO customers VALUES (?,?,?,?,?,?,?,?)", customer)
 
-# create Drivers table
-cursor.execute("""CREATE TABLE drivers (
-    driver_ID INTEGER PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT,
-    phone_number TEXT,
-    email_address TEXT,
-    branch TEXT,
-    active TEXT
-)
-""")
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
 
-# create Consignments table
-cursor.execute("""CREATE TABLE vans (
-    barcode TEXT PRIMARY KEY,
-    driver_ID INTEGER,
-    date_of_return TEXT,
-    active TEXT
-)
-""")
+def add_driver(driver):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
 
-# commit our command
-connection.commit()
-# close our connection
-connection.close()
+    # create a cursor
+    cursor = connection.cursor()
+
+    # add the customer to the table
+    cursor.execute("INSERT INTO drivers VALUES (?,?,?,?,?,?,?)", driver)
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+def add_van(van):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # add the customer to the table
+    cursor.execute("INSERT INTO vans VALUES (?,?,?,?,?,?,?)", van)
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+
+# UPDATE
+def update_customer_details(item_to_update,item, id_key):
+
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+    # create a cursor
+    cursor = connection.cursor()
+
+    # Record update
+    if item_to_update == 'phone_num':
+        cursor.execute("""UPDATE customers SET phone_number = %item WHERE customer_id = %id_key""" % (item, id_key))
+    elif item_to_update == 'email_address':
+        cursor.execute("""UPDATE customers SET email_address = %item WHERE customer_id = %id_key""" % (item, id_key))
+    elif item_to_update == 'address':
+        cursor.execute("""UPDATE customers SET address = %item WHERE customer_id = %id_key""" % (item, id_key))
+    elif item_to_update == 'geo_area':
+        cursor.execute("""UPDATE customers SET geo_area = %item WHERE customer_id = %id_key""" % (item, id_key))
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+
+
+
+
+
+
+
