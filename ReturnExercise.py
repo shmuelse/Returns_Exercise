f72c0b6cb34fc84acc5a63bb2d96dd180f668ccc
shmuelse <44799431+shmuelse@users.noreply.github.com>
2020-09-01 20:47:02 +0300

    on work

--- ReturnExercise.py
+++ ReturnExercise.py
@@ -16,13 +16,6 @@ connection = sqlite3.connect('returns.db')
 # create a cursor
 cursor = connection.cursor()
 
-
-
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
-available_table = (cursor.fetchall())
-print(available_table)
-
-
 # commit our command
 connection.commit()
 # close our connection
