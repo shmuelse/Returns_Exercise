20b2b1ef755c6f6bea410ab8767160f8ecc369ae
shmuelse <44799431+shmuelse@users.noreply.github.com>
2020-09-02 17:16:59 +0300

    work on CRUD

--- ReturnExercise.py
+++ ReturnExercise.py
@@ -60,7 +60,7 @@ def add_van(van):
     connection.close()
 
 
-def add_consignments(consignments):
+def add_consignment(consignment):
     # connect to database
     connection = sqlite3.connect('returns.db')
 
@@ -68,12 +68,84 @@ def add_consignments(consignments):
     cursor = connection.cursor()
 
     # add the customer to the table
-    cursor.execute("INSERT INTO vans VALUES (?,?,?,?)", consignments)
+    cursor.execute("INSERT INTO vans VALUES (?,?,?,?)", consignment)
 
     # commit our command
     connection.commit()
     # close our connection
     connection.close()
+
+
+# READ
+def get_all_drivers_from_branch(branch_):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # get all the drivers that work on a specific branch
+    cursor.execute("""
+    SELECT first_name, last_name 
+    FROM drivers 
+    WHERE branch = %(branch_)""", {"branch_": branch_})
+
+    to_ret = cursor.fetchall()
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+    return to_ret
+
+
+def get_driver_details_by_id(driver_id):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # get all driver driver details by driver id
+    cursor.execute("""
+       SELECT first_name, last_name, phone_number, email_address, branch 
+       FROM drivers 
+       WHERE driver_id = %(driver_id) 
+       AND active = 'Y' """, {"driver_id": driver_id})
+
+    to_ret = cursor.fetchall()
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+    return to_ret
+
+
+def get_drivers_details_by_first_name(driver_name):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # get all driver driver details by driver id
+    cursor.execute("""
+       SELECT first_name, last_name, phone_number, email_address, branch 
+       FROM drivers 
+       WHERE first_name = %(driver_name) 
+       AND active = 'Y' """, {"driver_name": driver_name})
+
+    to_ret = cursor.fetchall()
+
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+    return to_ret
 
 
 # UPDATE
@@ -91,7 +163,7 @@ def update_user_details(item_to_update, item, id_key, user_type):
             UPDATE customers 
             SET phone_number = %(item)s 
             WHERE customer_id = %(id_key)s 
-            AND active = 'Y'""", {'item': item, 'id_key': id_key} )
+            AND active = 'Y'""", {'item': item, 'id_key': id_key})
 
         elif item_to_update == 'email_address':
             cursor.execute("""
@@ -121,21 +193,21 @@ def update_user_details(item_to_update, item, id_key, user_type):
             UPDATE drivers 
             SET phone_number = %(item)s 
             WHERE driver_ID = %(id_key)s
-            AND active = 'Y'""", {'item': item, 'id_key':  id_key})
+            AND active = 'Y'""", {'item': item, 'id_key': id_key})
 
         elif item_to_update == 'email_address':
             cursor.execute("""
             UPDATE drivers 
             SET email_address = %(item)s 
             WHERE driver_ID = %(id_key)s
-            AND active = 'Y'""", {'item': item, 'id_key':  id_key})
+            AND active = 'Y'""", {'item': item, 'id_key': id_key})
 
         elif item_to_update == 'branch':
             cursor.execute("""
             UPDATE drivers 
             SET branch = %(item)s 
             WHERE driver_ID = %(id_key)s
-            AND active = 'Y'""", {'item': item, 'id_key':  id_key})
+            AND active = 'Y'""", {'item': item, 'id_key': id_key})
 
     # commit our command
     connection.commit()
@@ -143,16 +215,90 @@ def update_user_details(item_to_update, item, id_key, user_type):
     connection.close()
 
 
+def update_consignment_return_date(date, barcode_):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+    # create a cursor
+    cursor = connection.cursor()
+
+    cursor.execute("""
+                UPDATE consignments 
+                SET date_of_return = %(date)s 
+                WHERE barcode = %(barcode_)s 
+                AND active = 'Y'""", {'date': date, 'barcode_': barcode_})
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+# DELETE
 def delete_customer_from_db(customer_to_remove):
     # connect to database
     connection = sqlite3.connect('returns.db')
     # create a cursor
     cursor = connection.cursor()
+
+    # mark customer as no active
     cursor.execute("""
                UPDATE customers 
                SET active = 'N'
                WHERE customer_id = %(customer_to_remove)s
                AND active = 'Y'""", {'customer_to_remove': customer_to_remove})
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+def delete_driver_from_db(driver_to_delete):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+    # create a cursor
+    cursor = connection.cursor()
+
+    # mark driver as no active
+    cursor.execute("""
+                 UPDATE drivers 
+                 SET active = 'N'
+                 WHERE driver_ID = %(driver_to_delete)s
+                 AND active = 'Y'""", {'driver_to_delete': driver_to_delete})
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+def delete_van_from_db(van_to_delete):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+    # create a cursor
+    cursor = connection.cursor()
+
+    # mark van as no active
+    cursor.execute("""
+                 UPDATE drivers 
+                 SET active = 'N'
+                 WHERE van_ID = %(van_to_delete)s
+                 AND active = 'Y'""", {'driver_to_delete': van_to_delete})
+    # commit our command
+    connection.commit()
+    # close our connection
+    connection.close()
+
+
+def delete_consignment_from_db(consignment_to_delete):
+    # connect to database
+    connection = sqlite3.connect('returns.db')
+    # create a cursor
+    cursor = connection.cursor()
+
+    # mark consignment as no active
+    cursor.execute("""
+                   UPDATE consignments 
+                   SET barcode = 'N'
+                   WHERE van_ID = %(consignment_to_delete)s
+                   AND active != 'N'""", {'consignment_to_delete': consignment_to_delete})
     # commit our command
     connection.commit()
     # close our connection
