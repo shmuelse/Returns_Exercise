085d7cffc857884b8f563c0f796380df108037a4
shmuelse <44799431+shmuelse@users.noreply.github.com>
2020-09-03 23:52:40 +0300

    finish step 01 for geo_coding API

--- BL.py
+++ BL.py
@@ -30,6 +30,34 @@ def extract_geo_area(address_or_geo_area, data_type='json'):
     return geo_area.get("long_name")
 
 
+def extract_address(address_or_geo_area, data_type='json'):
+    end_point = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
+    params = {"address": address_or_geo_area, "key": apiKey}
+    url_params = urlencode(params)
+    url = f"{end_point}?{url_params}"
+    r = requests.get(url)
+    if r.status_code not in range(200, 299):
+        return {}
+    street_number = {}
+    route = {}
+    geo_area = {}
+    state = {}
+    zip_code = {}
+    try:
+        street_number = r.json()['results'][0]['address_components'][0]
+        route = r.json()['results'][0]['address_components'][1]
+        geo_area = r.json()['results'][0]['address_components'][3]
+        state = r.json()['results'][0]['address_components'][5]
+        zip_code = r.json()['results'][0]['address_components'][7]
+    except:
+        pass
+    return street_number.get("short_name"), route.get("short_name"), geo_area.get("short_name"), state.get("short_name"), zip_code.get("short_name")
+
+
+# s = extract_address("520 maple street brooklyn ny")
+# print(' '.join([str(elem) for elem in s]))
+
+
 def customer_id_gen():
     customer_id_gen.count += 1
     return customer_id_gen.count
@@ -79,6 +107,9 @@ def add_new_van(driver_id, geo_area, branch):
 
 def add_new_consignment(van_id, geo_area, branch):
     Dl.add_consignment([(consignment_id_and_barcode_gen(), van_id, geo_area, branch, 'Y')])
+
+
+add_new_driver('john', 'gross', '2026567700', "john@ldtglobal.com", 'Brooklyn')
 
 
 
